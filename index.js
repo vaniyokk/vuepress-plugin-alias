f593dab34329251289d65c3ee36c1a7faf1f0f2b
Ivan Kuzmin <kuzmin6969@gmail.com>
2018-12-15 20:38:16 +0200

    Add basic alias page generation

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+const { fs, path } = require('@vuepress/shared-utils');
+
+function getTemplate(path) {
+  const result = '<!DOCTYPE html>' +
+    '<html>' +
+    '<head>' +
+      '<meta charset="utf-8">' +
+      '<title>Redirecting...</title>' +
+      '<link rel="canonical" href="' + path + '">' +
+      '<meta http-equiv="refresh" content="0; url=' + path + '">' +
+    '</head>' +
+    '</html>';
+
+  return result;
+}
+
 module.exports = (options, ctx) => ({
   async generated (pagePaths) {
-    console.log(ctx)
+    const { pages, outDir } = ctx;
+
+    pages.filter(({frontmatter}) => {
+      return frontmatter.alias || frontmatter.aliases;
+    }).forEach(page => {
+      let aliases = page.frontmatter.alias || page.frontmatter.aliases;
+      if (!Array.isArray(aliases)) aliases = [aliases];
+      if (!aliases.length) return;
+
+      const content = getTemplate(page.path);
+
+      aliases.forEach(async alias => {
+        const aliasPagePath = path.resolve(outDir, alias);
+        await fs.outputFile(aliasPagePath, content);
+      });
+    })
   }
 })
